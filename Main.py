213f90ccde6c87d26abc866ef7871b46c934307b
HannLautaa <hannlautaa@gmail.com>
2025-10-27 19:47:51 +0700

    ganti keseluruhan

--- Main.py
+++ Main.py
@@ -1,26 +1,43 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import geopandas as gpd
+import matplotlib.pyplot as plt
+from utils.sidebar import sidebar
 
-st.set_page_config(
-    layout='wide'
-)
+st.set_page_config(layout='wide')
+
+# @st.cache_data
+# def load_data(file):
+#     if file.name.endswith('.csv'):
+#         return pd.read_csv(file, index_col=0)
+#     else:
+#         return pd.read_excel(file, index_col=0)
+
+# # with c1:
+# with st.sidebar:
+#     t = st.selectbox('Pilih Tahun', options=[2024, 2023, 2022, 2021, 2020, 2019])
+#     st.divider()
+
+#     with st.expander('Upload Data'):
+#         up_file = st.file_uploader('Upload CSV or Excel file', type=['csv', 'xlsx'])
+
+#     if up_file is not None:
+#         opt = ['Default', up_file.name]
+#     else:
+#         opt = ['Default']
+#     sel_ds = st.selectbox('Pilih Dataset', options=opt)
+
+#     # tentukan sumber data
+#     if sel_ds == 'Default':
+#         df = load_data(open('data/df.csv', 'rb'))
+#     else:
+#         df = load_data(up_file)
 
 @st.cache_data
-def load_data():
-    return pd.read_csv('data/df.csv', index_col=0)
-
-@st.cache_data
-def load_map_data(tahun):
-    df = pd.read_csv('data/df.csv', index_col=0)
+def load_map_data(df):
     df.index = df.index.str.replace(' ', '', regex=False)
     df.index = df.index.str.lower()
     df['jumlah kasus'] = df['jumlah kasus laki-laki'] + df['jumlah kasus perempuan']
-
-    df = df[df['tahun'] == tahun]
 
     gdf = gpd.read_file('data/gadm41_IDN_2.json')
     gdf = gdf[gdf['NAME_1'] == 'JawaBarat']
@@ -28,50 +45,92 @@ def load_map_data(tahun):
     merged = gdf.merge(df, left_on='NAME_2', right_index=True)
     return merged
 
-df = load_data()
-# map_data = load_map_data()
+sidebar()
+df = st.session_state.current_df
+t = st.session_state.t
+try:
+    sub_df = df[df['tahun'] == t]
+except:
+    sub_df = df
+    sub_df.set_index('KABUPATEN/KOTA', inplace=True)
+    col_1 = st.selectbox(c)
 
-with st.sidebar:
-    # k = st.selectbox('Pilih Kabupaten', df.index.unique())
-    t = st.selectbox('Pilih Tahun', df['tahun'].unique()[::-1])
+merged = load_map_data(sub_df)
 
-    df_filtered = df[(df['tahun'] == t)]
+with st.container(border=True):
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        with st.container(border=True):
+            st.write('DataFrame')
+            try:
+                st.dataframe(sub_df)
+            except:
+                st.dataframe(df)
 
 
-fig, ax = plt.subplots(figsize=(20, 4))
-c1, c2 = st.columns(2)
-with c1:
-    with st.container(border=True):
-        st.write('Dataframe')
-        st.dataframe(df_filtered)
+    def format_number(x):
+        return f'{x:,.0f}'
 
-with c2:
-    with st.container(border=True, height=480, vertical_alignment='center'):
-        st.markdown(
-            f"""
-            <div style="text-align:center; font-size:20px; font-weight:bold;">
-                Peta Sebaran Kasus TBC di Jawa Barat Tahun {t}
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-        map_data = load_map_data(t)
-        map_data.plot(
-            ax=ax,
-            cmap='YlOrRd',
-            legend=True,
-            column='jumlah kasus',
-            edgecolor='black'
-        )
-        ax.axis('off')
-        st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(20, 4))
+    fig2, ax2 = plt.subplots(figsize=(18, 3))
 
-c3, c4 = st.columns(2)
-with c3:
-    with st.container(border=True):
-        st.write(f'Jumlah Kasus TBC di Jawa Barat Tahun {t}')
-        st.bar_chart(data=df_filtered.reset_index(), x='kabupaten/kota', y=['jumlah kasus laki-laki', 'jumlah kasus perempuan'], stack=False)
-with c4:
-    with st.container(border=True):
-        st.write('Jumlah Kasus TBC Provinsi Jawa Barat')
-        st.bar_chart(data=df.groupby(['tahun']).agg({'jumlah kasus laki-laki': 'sum', 'jumlah kasus perempuan': 'sum'}).reset_index(), x='tahun', y=['jumlah kasus laki-laki', 'jumlah kasus perempuan'], stack=False)
+    with col2:
+        with st.container(border=True, height=480, vertical_alignment='center'):
+
+            st.write('Peta Sebaran Kasus TBC di Jawa Barat')
+            map_data = load_map_data(sub_df)
+            map_data.plot(
+                ax=ax,
+                cmap='YlOrRd',
+                legend=True,
+                column='jumlah kasus',
+                edgecolor='black',
+                legend_kwds={'shrink': 0.5}
+            )
+            ax.axis('off')
+            st.pyplot(fig)
+    
+    # with st.container(border=True):
+    with st.expander('Show Bar Chart'):
+            st.bar_chart(sub_df.drop(columns='tahun'), stack=False)
+
+with st.container(border=True):
+    opt = sub_df.index.tolist()
+    kab = st.selectbox('', options=opt, label_visibility="collapsed", placeholder='Pilih Kabupaten/Kota', index=None)
+    if kab is not None:
+        c1, c2 = st.columns(2)
+        kab_kecil = kab.lower().replace(" ", "")
+
+        with c1:
+            with st.container(border=True):
+                sub_df2 = sub_df[sub_df.index == kab]
+                st.markdown(f'## {kab}')
+
+                st.divider()
+                # with st.container(border=True):
+                st.markdown('#### Jumlah Kasus Laki-laki')
+                st.write(format_number(sub_df2['jumlah kasus laki-laki'].values[0]))
+
+                # with st.container(border=True):
+                st.markdown('#### Jumlah Kasus Perempuan')
+                st.write(format_number(sub_df2['jumlah kasus perempuan'].values[0]))
+
+        with c2:
+            # with st.container(border=True, vertical_alignment='center'):
+                map_data = load_map_data(sub_df)
+                a, b = map_data['jumlah kasus'].min(), map_data['jumlah kasus'].max()
+                map_data.plot(ax=ax2, color='lightgray', edgecolor='black')
+                map_data = map_data[map_data['NAME_2'] == kab_kecil]
+                map_data.plot(
+                    ax=ax2,
+                    cmap='YlOrRd',
+                    legend=True,
+                    column='jumlah kasus',
+                    edgecolor='black',
+                    legend_kwds={'shrink': 0.5},
+                    vmin=a,
+                    vmax=b
+                )
+                ax2.axis('off')
+                st.pyplot(fig2)
---
+++ utils/sidebar.py
@@ -0,0 +1,49 @@
+def sidebar():
+    import streamlit as st
+    import pandas as pd
+    import io
+
+    @st.cache_data
+    def load_data(file_bytes, file_type):
+        if file_type == 'csv':
+            return pd.read_csv(io.BytesIO(file_bytes), index_col=0)
+        else:
+            return pd.read_excel(io.BytesIO(file_bytes), index_col=0)
+
+    with st.sidebar:
+        t = st.session_state.get('t', 2024)
+        t = st.selectbox(
+            'Pilih Tahun',
+            options=[2024, 2023, 2022, 2021, 2020, 2019],
+            index=[2024, 2023, 2022, 2021, 2020, 2019].index(t),
+        )
+        st.divider()
+
+        with st.expander('Upload Data'):
+            up_file = st.file_uploader('Upload CSV or Excel file', type=['csv', 'xlsx'])
+
+            if up_file is not None:
+                st.session_state['uploaded_file_bytes'] = up_file.getvalue()
+                st.session_state['uploaded_file_name'] = up_file.name
+
+        if 'uploaded_file_bytes' in st.session_state:
+            opt = ['Default', st.session_state['uploaded_file_name']]
+        else:
+            opt = ['Default']
+
+        sel_ds = st.session_state.get('opt', 'Default')
+        try:
+            sel_ds = st.selectbox('Pilih Dataset', options=opt, index=opt.index(sel_ds))
+        except:
+            sel_ds = st.selectbox('Pilih Dataset', options=opt)
+
+        if sel_ds == 'Default':
+            df = load_data(open('data/df.csv', 'rb').read(), 'csv')
+        else:
+            name = st.session_state['uploaded_file_name']
+            file_type = 'csv' if name.endswith('.csv') else 'xlsx'
+            df = load_data(st.session_state['uploaded_file_bytes'], file_type)
+
+    st.session_state['t'] = t
+    st.session_state['current_df'] = df
+    st.session_state['opt'] = sel_ds
